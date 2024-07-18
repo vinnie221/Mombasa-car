0e8fc3e371b32eb17876f8204eab46db6c012ffc
vinnie221 <murimivincent26@gmail.com>
2024-07-18 15:32:52 +0300

    fixing home

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import Logo from "./logo";
+import Logo from "./logo"; // Use uppercase for the component name
 import { FiSearch } from "react-icons/fi";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { FaWhatsappSquare } from "react-icons/fa";
@@ -12,16 +12,18 @@ import ROLE from "../common/role";
 import Context from "../context";
 import { LiaHandPointRightSolid } from "react-icons/lia";
 
+
 const Header = () => {
   const user = useSelector((state) => state?.user?.user);
   const dispatch = useDispatch();
   const [menuDisplay, setmenuDisplay] = useState(false);
-  const context = useContext(Context);
-  const navigate = useNavigate();
-  const searchInput = useLocation();
-  const URLSearch = new URLSearchParams(searchInput?.search);
-  const searchQuery = URLSearch.getAll("q");
-  const [search, setSearch] = useState(searchQuery);
+  const context = useContext(Context)
+  const navigate = useNavigate()
+  const searchInput = useLocation()
+  const URLSearch = new URLSearchParams(searchInput?.search)
+  const searchQuery = URLSearch.getAll("q")
+  const [search,setSearch] = useState(searchQuery)
+
 
   const handleLogout = async () => {
     const fetchData = await fetch(SummaryApi.logout_user.url, {
@@ -34,55 +36,61 @@ const Header = () => {
     if (data.success) {
       toast.success(data.message);
       dispatch(setUserDetails(null));
-      navigate("/");
+      navigate("/")
     }
     if (data.error) {
       toast.error(data.message);
     }
   };
+ 
+  const handleSearch = (e)=>{
+    const { value } = e.target
+    setSearch(value)
 
-  const handleSearch = (e) => {
-    const { value } = e.target;
-    setSearch(value);
-
-    if (value) {
-      navigate(`/search?q=${value}`);
-    } else {
-      navigate("/search");
+    if(value){
+      navigate(`/search?q=${value}`)
+    }else{
+      navigate("/search")
     }
-  };
 
-  const handleSearchAuto = (e) => {
-    const { value } = e.target;
-    setSearch(value);
 
-    if (value) {
-      navigate(`/search-auto?q=${value}`);
-    } else {
-      navigate("/search-auto");
+  }
+
+  const handleSearchAuto = (e)=>{
+    const { value } = e.target
+    setSearch(value)
+
+    if(value){
+      navigate(`/search-auto?q=${value}`)
+    }else{
+      navigate("/search-auto")
     }
-  };
+
+
+  }
+
 
   return (
     <header className="h-16 shadow-md bg-white fixed w-full z-40">
-      <div className="h-full container mx-auto flex items-center px-4 justify-between">
-        <div className="flex gap-6 items-center">
+      <div className="h-full container mx-auto flex items-center px-4, justify-between">
+        <div className="flex gap-6">
           <div>
             <Link to={"/"}>
               <Logo w={100} h={60} />
-            </Link>
+            </Link>{" "}
           </div>
-
+          
           <div className="">
-            <a
-              href="https://wa.me/254702751085"
-              target="_blank"
+            <a 
+              href="https://wa.me/254702751085" 
+              target="_blank" 
               rel="noopener noreferrer"
               className="text-green-400 text-4xl hover:text-green-700 hover:scale-150"
             >
               <FaWhatsappSquare />
             </a>
           </div>
+          {/* Use uppercase for the component name */}
         </div>
 
         <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2">
@@ -91,6 +99,7 @@ const Header = () => {
             placeholder="Search for products here..."
             className="w-full outline-none"
             onChange={handleSearchAuto}
+            //onChange = {handleSearchAuto}
             value={search}
           />
           <div className="text-lg min-w-[50px] h-8 bg-blue-600 flex items-center justify-center rounded-r-full text-white">
@@ -122,13 +131,22 @@ const Header = () => {
                 <nav>
                   {user?.role === ROLE.ADMIN && (
                     <div className="">
+                        {/* <div>
+                            <Link
+                            to={"/admin-panel/all-products"}
+                            className="whitespace-nowrap hover:bg-red-500 p-2  rounded-xl"
+                            onClick={() => setmenuDisplay((preve) => !preve)}
+                          >
+                            Products     Admin
+                          </Link>
+                      </div> */}
                       <div className="pt-2">
-                        <Link
+                        <Link 
                           className="whitespace-nowrap hover:bg-blue-500 p-2 rounded-xl"
                           to={"/automobileadmin-panel/all-uploads"}
                           onClick={() => setmenuDisplay((preve) => !preve)}
                         >
-                          Admin Section
+                        Admin Section
                         </Link>
                       </div>
                     </div>
@@ -137,6 +155,22 @@ const Header = () => {
               </div>
             )}
           </div>
+
+          {
+           
+                //     <Link to={"/cart"} className="text-2xl relative">
+                //         <span><FaShoppingCart /></span>
+
+                //         <div className="bg-red-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-3 -right-1.5">
+                //           <p className="text-sm">{context?.cardProductCount}</p>
+                //         </div>
+                // </Link>
+            
+          }
+
+          
+
+          
 
           <div>
             {user?._id ? (
@@ -154,21 +188,6 @@ const Header = () => {
                 Login
               </Link>
             )}
-          </div>
-        </div>
-      </div>
-
-      <div className="lg:hidden flex items-center justify-between px-4 mt-2">
-        <div className="flex items-center w-full border rounded-full focus-within:shadow pl-2">
-          <input
-            type="text"
-            placeholder="Search for products here..."
-            className="w-full outline-none"
-            onChange={handleSearchAuto}
-            value={search}
-          />
-          <div className="text-lg min-w-[50px] h-8 bg-blue-600 flex items-center justify-center rounded-r-full text-white">
-            <FiSearch />
           </div>
         </div>
       </div>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,38 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { LiaHandPointRightSolid } from "react-icons/lia";
 import BannerProduct from '../components/BannerProduct'
 
 import CategoryListAuto from '../components/CategoryListAuto'
 import VerticalCardProductAuto from '../components/VerticalCardProductAuto'
-import { Link, useNavigate } from 'react-router-dom';
-import { FiSearch } from 'react-icons/fi';
-const navigate = useNavigate()
-const [search,setSearch] = useState(searchQuery)
-
+import { Link } from 'react-router-dom';
 
 
 const Home = () => {
-  
   return (
     <div>
       <div className='flex w-full justify-between p-2 pl-5 pb-0'>
         <Link to={'/about-us'}>
           <p className="bg-blue-500 w-28 h-10 justify-center items-center rounded-xl text-white hover:bg-blue-800 flex">about us</p>
         </Link>
-
-        <div className="hidden lg:block items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2">
-          <input
-            type="text"
-            placeholder="Search here..."
-            className="w-full outline-none"
-            onChange={handleSearchAuto}
-            //onChange = {handleSearchAuto}
-            value={search}
-          />
-          {/* <div className="text-lg min-w-[50px] h-8 bg-blue-600 flex items-center justify-center rounded-r-full text-white">
-            <FiSearch />
-          </div> */}
-        </div>
+      
         
         
         <div className='w-full flex justify-end'>
