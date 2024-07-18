f65475072a652b61b4715a0daa366014c54a3c5b
vinnie221 <murimivincent26@gmail.com>
2024-07-18 15:19:58 +0300

    fixing logo error 2

--- frontend/src/components/logo.js
+++ frontend/src/components/logo.js
@@ -2,20 +2,23 @@ import React from 'react';
 import { FaCarSide } from "react-icons/fa";
 import { GiIsland } from "react-icons/gi";
 
-const Logo = ({ w, h }) => {
+const Logo = ({w, h}) => {
   return (
     <div className='flex gap-1 bg-slate-300 p-2 rounded hover:bg-slate-600'>
-      <div className='text-white justify-center text-center font-semibold rounded-xl items-center bg-red-600 w-12 lg:w-20 text-xs lg:text-base'> {/* Adjusted width and text size */}
+      <div className='text-white justify-center text-center font-semibold rounded-xl items-center bg-red-600 w-20'>
         Home
       </div>
-      <div className='text-xl lg:text-2xl text-red-600'> {/* Adjusted icon size */}
-        <FaCarSide />
+      <div className='text-2xl text-red-600'>
+      <FaCarSide />
       </div>
-      <div className='text-xl lg:text-2xl text-green-500'> {/* Adjusted icon size */}
-        <GiIsland />
+      <div className='text-2xl text-green-500'>
+      <GiIsland />
       </div>
     </div>
+    
+    
   );
 };
 
 export default Logo;
+
