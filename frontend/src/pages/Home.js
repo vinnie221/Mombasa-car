a88a8d51ea03ba3a5881bce5dd3637ca563f5bef
vinnie221 <murimivincent26@gmail.com>
2024-07-18 17:10:27 +0300

    flex col

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -27,24 +27,27 @@ const Home = () => {
   return (
     <div>
       <div className='flex w-full justify-between p-2 pl-5 pb-0'>
-        <Link to={'/about-us'}>
-          <p className="bg-blue-500 w-28 h-10 justify-center items-center rounded-xl text-white hover:bg-blue-800 flex">about us</p>
-        </Link>
-        {/* Mobile search bar */}
-      <div className="block lg:hidden px-4 my-4">
-        <div className="relative flex items-center w-full border rounded-full focus-within:shadow pl-2">
-          <input
-            type="text"
-            placeholder="Search for products here..."
-            className="w-1/4 outline-none text-sm px-3 py-1"
-            onChange={handleSearchAuto}
-            value={search}
-          />
-          <div className="text-lg min-w-[50px] h-8 bg-blue-600 flex items-center justify-center rounded-r-full text-white">
-            <FiSearch />
+        <div className='flex-col'>
+          <Link to={'/about-us'}>
+            <p className="bg-blue-500 w-28 h-10 justify-center items-center rounded-xl text-white hover:bg-blue-800 flex">about us</p>
+          </Link>
+          {/* Mobile search bar */}
+        <div className="block lg:hidden px-4 my-4">
+          <div className="relative flex items-center w-full border rounded-full focus-within:shadow pl-2">
+            <input
+              type="text"
+              placeholder="Search for products here..."
+              className="w-1/4 outline-none text-sm px-3 py-1"
+              onChange={handleSearchAuto}
+              value={search}
+            />
+            <div className="text-lg min-w-[50px] h-8 bg-blue-600 flex items-center justify-center rounded-r-full text-white">
+              <FiSearch />
+            </div>
           </div>
         </div>
-      </div>
+        </div>
+        
         
         <div className='w-full flex justify-end'>
           <div className=''>
