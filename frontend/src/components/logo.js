e737e1c3d707d86d7d24f15b76ead7795b88d6c0
vinnie221 <murimivincent26@gmail.com>
2024-07-18 14:37:37 +0300

    adjust logo size

--- frontend/src/components/logo.js
+++ frontend/src/components/logo.js
@@ -5,20 +5,17 @@ import { GiIsland } from "react-icons/gi";
 const Logo = ({w, h}) => {
   return (
     <div className='flex gap-1 bg-slate-300 p-2 rounded hover:bg-slate-600'>
-      <div className='text-white justify-center text-center font-semibold rounded-xl items-center bg-red-600 w-20'>
+      <div className='text-white justify-center text-center font-semibold rounded-xl items-center bg-red-600 w-12 lg:w-20 text-xs lg:text-base'> {/* Adjusted width and text size */}
         Home
       </div>
-      <div className='text-2xl text-red-600'>
-      <FaCarSide />
+      <div className='text-xl lg:text-2xl text-red-600'> {/* Adjusted icon size */}
+        <FaCarSide />
       </div>
-      <div className='text-2xl text-green-500'>
-      <GiIsland />
+      <div className='text-xl lg:text-2xl text-green-500'> {/* Adjusted icon size */}
+        <GiIsland />
       </div>
     </div>
-    
-    
   );
 };
 
 export default Logo;
-
