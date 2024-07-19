35a69e4212244d5c3c3a2da24c3912cb03f3000b
vinnie221 <murimivincent26@gmail.com>
2024-07-19 14:39:29 +0300

    updated time display

--- frontend/src/helpers/displayFormattedTimestamp.js
+++ frontend/src/helpers/displayFormattedTimestamp.js
@@ -5,12 +5,18 @@ const displayFormattedTimestamp = (timestamp) => {
     const hours = Math.floor(timeDifference / (1000 * 60 * 60));
     const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
-    if (minutes < 60) {
+    if (minutes === 0) {
+        return 'just now';
+    } else if (minutes < 60) {
         return `${minutes} minute${minutes !== 1 ? 's' : ''} ago`;
     } else if (hours < 24) {
         return `${hours} hour${hours !== 1 ? 's' : ''} ago`;
-    } else {
+    } else if (days <= 2) {
         return `${days} day${days !== 1 ? 's' : ''} ago`;
+    } else {
+        const date = new Date(timestamp);
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        return date.toLocaleDateString(undefined, options);
     }
 };
 
