619286a177fcd121e8fce770bdf1fa10bf5ca80e
vinnie221 <murimivincent26@gmail.com>
2024-07-29 15:33:37 +0300

    SEO helmet

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,11 +7,13 @@ import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 import { Provider } from 'react-redux'
 import { store } from './stores/store';
+import { HelmetProvider } from 'react-helmet-async';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store }>
+      <HelmetProvider/>
       <RouterProvider router={router} />
     </Provider>
 
