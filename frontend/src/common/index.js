7223f2c1dfe788637a390edb6a601677d03b5f09
vinnie221 <murimivincent26@gmail.com>
2024-08-26 16:12:24 +0300

    forgot frontend url

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -4,7 +4,7 @@
 
 //import allUsers from "../../../backend/controller/allUsers"
 
-const backendDomain = "http://localhost:8080"  // process.env.REACT_APP_BACKEND_URL //"http://localhost:8080"
+const backendDomain =  process.env.REACT_APP_BACKEND_URL //"http://localhost:8080"
 
 const SummaryApi = {
     SignUp : {
